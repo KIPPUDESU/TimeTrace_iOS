0a8fc9853be31ce3c928facc398330796112d372
切符 <KIPPUDESU@OUTLOOK.com>
2026-08-02 00:36:10 +0800

    Initial Commit

---
+++ TimeTrace-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeTrace-iOS
+//
+//  Created by 切符 on 2026/8/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeTrace-iOS/TimeTrace_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrace_iOSApp.swift
+//  TimeTrace-iOS
+//
+//  Created by 切符 on 2026/8/2.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrace_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
